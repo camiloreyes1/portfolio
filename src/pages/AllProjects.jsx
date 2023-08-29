863c99bbdf103ba489605276456dd9645f02921e
camiloreyes1 <jreye236@fiu.edu>
2023-08-29 17:05:56 -0400

    commit

--- src/pages/AllProjects.jsx
+++ src/pages/AllProjects.jsx
@@ -5,78 +5,62 @@ import img2 from '../images/MRKT.png';
 import img3 from '../images/chicken-run.png'
 import Carousel from 'react-bootstrap/Carousel';
 
-
-
 const AllProjects = () => {
 
-    
+
   return (
-    <div id='all-projects'>
-        
-        <div>
-
-          
-
-        <Link to="https://project-3-client-roan.vercel.app/"> Pixels </Link>
-
-        <p>Pixels is a photo sharing app that lets users like and comment on other people's pictures. <br/>
-          This project is a full-stack app that was made using many diffent tools such as React, <br/>
-          Express, MongoDB, Bootstrap, and JavaScript. </p>
-
-        </div>
-
-        <br/>
-        <div>
-        <Link to="https://mrkt.adaptable.app/"> MRKT </Link>
-        <p>MRKT is a trading app that lets users post anything they would like to sell while letting<br/>
-          potential sellers message the owner about details about such product. This project was achieved <br/>
-          using tools such as Express, Bootstrap, Handlebars, and JavaScript </p>
+    <div>
 
 
-        </div>
+      <br/>
 
-        <br/>
-        
-        <div>
-        <Link to="https://camiloreyes1.github.io/project-1/">Chicken Run</Link>
-        <p>
-          Chicken Run is a game that consists of helping the chicken cross the road without getting runover<br/>
+      <div>
+
+        <Card style={{ width: '40rem' }} data-bs-theme="dark">
+          <Card.Img variant="top" src={img1} />
+          <Card.Body>
+            <Card.Text>
+            Pixels is a photo sharing app that lets users like and comment on other people's pictures. <br />
+          This project is a full-stack app that was made using many diffent tools such as React, <br />
+          Express, MongoDB, Bootstrap, and JavaScript.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <br></br>
+
+        <Card style={{ width: '40rem' }} data-bs-theme="dark">
+          <Card.Img variant="top" src={img2} />
+          <Card.Body>
+            <Card.Text>
+            Pixels is a photo sharing app that lets users like and comment on other people's pictures. <br />
+          This project is a full-stack app that was made using many diffent tools such as React, <br />
+          Express, MongoDB, Bootstrap, and JavaScript.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <br></br>
+
+        <Card style={{ width: '40rem' }} data-bs-theme="dark">
+          <Card.Img variant="top" src={img3} />
+          <Card.Body>
+            <Card.Text>
+            Chicken Run is a game that consists of helping the chicken cross the road without getting runover<br />
           less than three times. This project was done using JavaScript, DOM maniputalion, HTML5, and CSS
-        </p>
-
-        <Carousel fade data-bs-theme="dark">
-      <Carousel.Item interval={2500}>
-        <img class="img-fluid" src={img1} text="First slide" />
-        <Carousel.Caption>
-          <h3>Pixels</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={2500}>
-      <img  class="img-fluid" src={img2} text="First slide" />        
-      <Carousel.Caption>
-          <h3>MRKT</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-      <img class="img-fluid" src={img3} text="First slide" />        
-      <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-
-      
+            </Card.Text>
+          </Card.Body>
+        </Card>
 
 
-        </div>
 
-        
-       
+
+
+
+      </div>
+
+
+
     </div>
   )
 }
